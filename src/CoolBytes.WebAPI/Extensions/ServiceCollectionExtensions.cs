94c5012361ef5a83bd338f5b3e0cb5009e89f825
Tom <tombina@outlook.com>
2019-04-01 21:22:35 +0200

    clean up startup cs

--- src/CoolBytes.WebAPI/Extensions/ServiceCollectionExtensions.cs
+++ src/CoolBytes.WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -2,11 +2,61 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
+using CoolBytes.Core.Attributes;
+using CoolBytes.Core.Interfaces;
+using CoolBytes.Services.Caching;
+using CoolBytes.WebAPI.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 
 namespace CoolBytes.WebAPI.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddSecurity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddCors(o =>
+            {
+                o.AddPolicy("ProductionPolicy", builder => builder.WithOrigins("http://coolbytes.io", "http://www.coolbytes.io").AllowAnyMethod().AllowAnyHeader().Build());
+                o.AddPolicy("DevPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().Build());
+            });
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.Authority = config["Auth0:Domain"];
+                o.Audience = config["Auth0:ApiIdentifier"];
+            });
+
+            services.AddAuthorization(o =>
+            {
+                o.AddPolicy("admin",
+                    policy => policy.Requirements.Add(new HasScopeRequirement("admin", config["Auth0:Domain"])));
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection ScanServices(this IServiceCollection services)
+        {
+            services.Scan(s =>
+                s.FromAssemblyOf<ICacheService>()
+                    .AddClasses(c => c.WithAttribute<ScopedAttribute>())
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime());
+
+            services.Scan(s =>
+                s.FromAssemblyOf<IImageFactory>()
+                    .AddClasses(c => c.WithAttribute<ScopedAttribute>())
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime());
+
+            return services;
+        }
+
         public static IServiceCollection AddMailgun(this IServiceCollection services)
         {
             services.AddSingleton<MailgunMailerOptionsFactory, MailgunMailerOptionsFactory>();
--- src/CoolBytes.WebAPI/Startup.cs
+++ src/CoolBytes.WebAPI/Startup.cs
@@ -1,15 +1,11 @@
 ﻿using AutoMapper;
-using CoolBytes.Core.Attributes;
 using CoolBytes.Core.Builders;
 using CoolBytes.Core.Factories;
 using CoolBytes.Core.Interfaces;
 using CoolBytes.Data;
-using CoolBytes.Services.Caching;
-using CoolBytes.WebAPI.Authorization;
 using CoolBytes.WebAPI.Extensions;
 using FluentValidation.AspNetCore;
 using MediatR;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -23,67 +19,27 @@ namespace CoolBytes.WebAPI
     {
         private readonly IConfiguration _configuration;
 
-        public Startup(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) 
+            => _configuration = configuration;
 
         public void ConfigureServices(IServiceCollection services)
         {
-            ConfigureSecurity(services);
-
             services.AddHttpClient();
             services.AddHttpContextAccessor();
             services.AddTransient<BlogPostBuilder>();
             services.AddTransient<ExistingBlogPostBuilder>();
-
-            services.Scan(s =>
-                s.FromAssemblyOf<ICacheService>()
-                    .AddClasses(c => c.WithAttribute<ScopedAttribute>())
-                    .AsImplementedInterfaces()
-                    .WithScopedLifetime());
-
-            services.Scan(s =>
-                s.FromAssemblyOf<IImageFactory>()
-                    .AddClasses(c => c.WithAttribute<ScopedAttribute>())
-                    .AsImplementedInterfaces()
-                    .WithScopedLifetime());
-
-            services.AddScoped<IImageFactoryOptions>(sp => new ImageFactoryOptions(_configuration["ImagesUploadPath"]));
-
+            services.AddSecurity(_configuration);
             services.AddDbContextPool<AppDbContext>(o => o.UseSqlServer(_configuration.GetConnectionString("Default")));
             services.AddMvc()
-                        .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                        .AddJsonOptions(o => o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
-                        .AddFluentValidation(config => config.RegisterValidatorsFromAssembly(typeof(Startup).Assembly));
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(o => o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddFluentValidation(config => config.RegisterValidatorsFromAssembly(typeof(Startup).Assembly));
             services.AddMediatR(typeof(Startup));
             services.AddSwaggerDocument();
+
+            services.ScanServices();
+            services.AddScoped<IImageFactoryOptions>(sp => new ImageFactoryOptions(_configuration["ImagesUploadPath"]));
             services.AddMailgun();
-        }
-
-        private void ConfigureSecurity(IServiceCollection services)
-        {
-            services.AddCors(o =>
-            {
-                o.AddPolicy("ProductionPolicy", builder => builder.WithOrigins("http://coolbytes.io", "http://www.coolbytes.io").AllowAnyMethod().AllowAnyHeader().Build());
-                o.AddPolicy("DevPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().Build());
-            });
-
-            services.AddAuthentication(o =>
-            {
-                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(o =>
-            {
-                o.Authority = _configuration["Auth0:Domain"];
-                o.Audience = _configuration["Auth0:ApiIdentifier"];
-            });
-
-            services.AddAuthorization(o =>
-            {
-                o.AddPolicy("admin",
-                    policy => policy.Requirements.Add(new HasScopeRequirement("admin", _configuration["Auth0:Domain"])));
-            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
