ffff8fec459240cc4b5849f60e3b1e148c809ec1
Tom <tombina@outlook.com>
2019-04-27 11:01:45 +0200

    blobclient

--- CoolBytes.Services/ImageFactories/AzureBlobImageFactory.cs
+++ CoolBytes.Services/ImageFactories/AzureBlobImageFactory.cs
@@ -1,48 +1,35 @@
-﻿using CoolBytes.Core.Attributes;
+﻿using CoolBytes.Core.Abstractions;
+using CoolBytes.Core.Attributes;
 using CoolBytes.Core.Domain;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using CoolBytes.Services.BlobStorage;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using CoolBytes.Core.Abstractions;
-using Microsoft.Azure.Storage;
-using Microsoft.Azure.Storage.Blob;
 
 namespace CoolBytes.Services.ImageFactories
 {
     [Inject(typeof(ImageFactory), ServiceLifetime.Scoped, "development", "production-azure")]
     public class AzureBlobImageFactory : ImageFactory
     {
-        private readonly IHostingEnvironment _environment;
-        private readonly string _connectionString;
+        private readonly IBlobClient _client;
 
-        public AzureBlobImageFactory(IConfiguration configuration, IHostingEnvironment environment, IImageFactoryValidator validator) : base(validator)
+        public AzureBlobImageFactory(IBlobClient client, IImageFactoryValidator validator) : base(validator)
         {
-            _environment = environment;
-            _connectionString = configuration.GetConnectionString("BlobStorage").Replace("{KEY}", configuration["storagekey"]);
+            _client = client;
         }
 
         public override async Task<Image> Create(Stream stream, string currentFileName, string contentType)
         {
             Validate(stream, currentFileName, contentType);
 
-            var blobRef = CreateBlobReference(currentFileName);
-            await blobRef.UploadFromStreamAsync(stream);
-
-            return new Image(blobRef.Name, blobRef.Container.Name, blobRef.Name, blobRef.Properties.Length, contentType);
-        }
-
-        private CloudBlockBlob CreateBlobReference(string currentFileName)
-        {
-            var container = _environment.EnvironmentName.ToLower();
-            var client = CloudStorageAccount.Parse(_connectionString).CreateCloudBlobClient();
-            var containerRef = client.GetContainerReference(container);
             var fileName = $"{Guid.NewGuid().ToString()}{Path.GetExtension(currentFileName)}";
-            var blobRef = containerRef.GetBlockBlobReference(fileName);
+            var blob = _client.Create(fileName);
+            blob.Properties.ContentType = contentType;
 
-            return blobRef;
+            await blob.UploadFromStreamAsync(stream);
+            
+            return new Image(blob.Name, blob.Container.Name, blob.Name, blob.Properties.Length, contentType);
         }
     }
 }
