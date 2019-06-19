47385b6fd26e08de1f7568bfa1c63ebc2c284c33
Tom Bina <31348448+TomBina@users.noreply.github.com>
2019-06-19 22:58:07 +0200

    added extra cors host (#43)

--- src/CoolBytes.WebAPI/Extensions/ServiceCollectionExtensions.cs
+++ src/CoolBytes.WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -24,7 +24,7 @@ namespace CoolBytes.WebAPI.Extensions
             services.AddCors(o =>
             {
                 o.AddPolicy("ProductionPolicy", builder => 
-                    builder.WithOrigins("http://coolbytes.io", "http://www.coolbytes.io", "http://localhost:4000",
+                    builder.WithOrigins("https://www.coolbytes.io", "http://localhost:4000",
                         "http://staging.coolbytes.io").AllowAnyMethod().AllowAnyHeader().Build());
                 o.AddPolicy("DevPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().Build());
             });
