926cdc9fce577aac51de4d590cf1068a11a80063
Tom <tombina@outlook.com>
2019-08-14 11:28:16 +0200

    categoryviewmodel

--- src/CoolBytes.WebAPI/Features/Categories/ViewModels/CategoryViewModel.cs
+++ src/CoolBytes.WebAPI/Features/Categories/ViewModels/CategoryViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace CoolBytes.WebAPI.Features.Categories.ViewModels
+﻿using AutoMapper;
+using CoolBytes.Core.Domain;
+
+namespace CoolBytes.WebAPI.Features.Categories.ViewModels
 {
+    [AutoMap((typeof(Category)))]
     public class CategoryViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int SortOrder { get; set; }
         public string Description { get; set; }
+        public bool IsCourse { get; set; }
     }
 }
